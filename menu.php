33602e80d9f7b845f7c336ebcac70744cf97378c
srihitha Pulapa <124449649+srihitha1234@users.noreply.github.com>
2024-05-14 22:40:18 +0530

    added view and update options to menu

--- menu.php
+++ menu.php
@@ -13,7 +13,8 @@
                     <li><a href="index.php">Home</a></li> 
                     <li><a href="">Student Login</a></li>
                     <li><a href="manage-admin.php">Admin</a></li>
-                    <li><a href="manage-mess-menu.php">Mess Menu</a></li>
+                    <li><a href = ""> View Mess Menu </a></li>
+                    <li><a href="manage-mess-menu.php">Update Mess Menu</a></li>
                     <li><a href="manage-about.php">About</a></li>
                     <li><a href="manage-contact.php">Contact Us</a></li>                                          
                 </ul>     
